3b8c8fcc21c3f48d9f45ebd844a03bc260b493dc
Oshannethsara <nethsaraoshan6@gmail.com>
2025-01-26 18:55:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || "put your github usearname",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&TEsyjQbQ#bB2YW1jcXjajctiAygIZEq-7kMkY2z6spNU_O_ifGRg",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Oshannethsara",
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "put your github auth token without ghp_",
 };
